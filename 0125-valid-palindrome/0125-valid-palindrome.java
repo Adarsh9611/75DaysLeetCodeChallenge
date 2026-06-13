5079c8e78724a2276537524b22766d6a5b59541d
Adarsh9611 <adarsh.23cs003@sode-edu.in>
2026-06-13 18:12:01 +0530

    Time: 2 ms (99.19%), Space: 44.3 MB (65.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,43 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            // Skip non-alphanumeric characters
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+        int left=-0;
+        int right=s.length()-1;
+        while(left<right)
+        {
+            while(left<right&&!isAplhaNumeric(s.charAt(left)))
+            {
                 left++;
             }
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+             while(left<right&&!isAplhaNumeric(s.charAt(right)))
+            {
                 right--;
             }
-
-            // Compare characters (case-insensitive)
-            if (Character.toLowerCase(s.charAt(left)) != 
-                Character.toLowerCase(s.charAt(right))) {
+            if(toLower(s.charAt(left))!=toLower(s.charAt(right)))
+            {
                 return false;
             }
-
             left++;
             right--;
         }
-
         return true;
     }
+    public boolean isAplhaNumeric(char ch)
+    {
+        if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'||ch>='0'&&ch<='9')
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public char toLower(char ch)
+    {
+        if(ch>='A'&&ch<='Z')
+        {
+            return (char)(ch+32);
+        }
+         return ch;
+    }
+   
 }
